8243fa5b4a3ccd56776982bbbca2bc54eb31e4ef
Ahdak <ahmed.dammak.net@gmail.com>
2018-05-16 18:30:17 +0200

    Init Project

---
+++ enhunt/src/main/java/com/marf/enhunt/EnhuntApplication.java
@@ -0,0 +1,12 @@
+package com.marf.enhunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnhuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnhuntApplication.class, args);
+	}
+}
---
+++ enhunt/src/test/java/com/marf/enhunt/EnhuntApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marf.enhunt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EnhuntApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
